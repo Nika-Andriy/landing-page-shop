e602d7d47748b34cc40e2045457bae81b2afeb63
Andriy <andriit35@gmail.com>
2026-09-08 11:44:42 +0300

    make build

---
+++ index.7d1cb9aa.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".icon--phone"),s=document.querySelector(".icon__number"),t=document.querySelector(".top-bar");e.addEventListener("mouseenter",()=>{s.classList.add("is-visible")}),t.addEventListener("mouseleave",()=>{s.classList.remove("is-visible")});
+//# sourceMappingURL=index.7d1cb9aa.js.map
---
+++ index.d2f25965.js
@@ -0,0 +1,2 @@
+var e,n,t;e=document.querySelector(".icon--phone"),n=document.querySelector(".icon__number"),t=document.querySelector(".top-bar"),e.addEventListener("mouseenter",function(){n.classList.add("is-visible")}),t.addEventListener("mouseleave",function(){n.classList.remove("is-visible")});
+//# sourceMappingURL=index.d2f25965.js.map
